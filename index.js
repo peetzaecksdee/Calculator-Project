58a8b8c4233c349e1c48f204c47cc18c589ae5d7
peetzaecksdee <peetzaecksdee@hotmail.com>
2022-10-15 22:09:30 +0700

    Finished the project

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-function operate(operator, numOne, numTwo) {
-  switch (operator) {
-    case "+":
-      result = add(numOne, numTwo);
-      break;
-    case "-":
-      result = subtract(numOne, numTwo);
-      break;
-    case "*":
-      result = multiply(numOne, numTwo);
-      break;
-    case "/":
-      result = divide(numOne, numTwo);
-      break;
-  }
-}
-
-let operator,
-  result,
-  number = "0";
+let operator;
+let result = 0;
+let number = "0";
 const buttons = document.querySelectorAll("button");
 const textDisplay = document.querySelector(".display");
+
+window.addEventListener("keydown", (e) => handleKeyboard(e));
 
 buttons.forEach((Btn) => {
   Btn.addEventListener("click", (e) => clickHandler(e.target.value));
 });
 
-function clickHandler(e, type) {
-  if (isNaN(e)) {
+function clickHandler(e) {
+  if (isNaN(e) && e !== ".") {
     handleSymbol(e);
   } else {
     handleNumber(e);
@@ -47,10 +27,17 @@ function clear() {
 }
 
 function handleCalculation(symbol) {
-  if (number === 0) return;
+  if (number === "0") return;
 
-  const anotherNumber = number;
-  return;
+  const anotherNumber = parseInt(number);
+
+  if (result === 0) {
+    result = anotherNumber;
+  } else {
+    mathCalculation(anotherNumber);
+  }
+  operator = symbol;
+  number = "0";
 }
 
 function handleSymbol(symbol) {
@@ -61,15 +48,71 @@ function handleSymbol(symbol) {
     case "abs":
       number = number.startsWith("-") ? number.replace("-", "") : `-${number}`;
       break;
-    case "percent":
-      number = 
+    case "%":
+      number *= 0.01;
+      break;
+    case "=":
+      evaluate();
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      handleCalculation(symbol);
+      break;
   }
+}
+
+function evaluate() {
+  if (operator === null) return;
+  operate(parseInt(number));
+  operator = null;
+  number = result;
+  result = 0;
 }
 
 function handleNumber(stringNumber) {
   if (number === "0") {
     number = stringNumber;
   } else {
-    number += stringNumber;
+    if (stringNumber !== "." || !number.includes(".")) {
+      number += stringNumber;
+    }
   }
+}
+
+function operate(anotherNumber) {
+  if (operator === "+") {
+    result += anotherNumber;
+  } else if (operator === "-") {
+    result -= anotherNumber;
+  } else if (operator === "*") {
+    result *= anotherNumber;
+  } else if (operator === "/") {
+    if (anotherNumber === 0) {
+      result = "Why?";
+      return;
+    }
+    result /= anotherNumber;
+  }
+  textDisplay.textContent = Math.round(number * 1000) / 1000;
+}
+
+function deleteNumber() {
+  number = number.slice(0, -1);
+}
+
+function handleKeyboard(e) {
+  if ((e.key >= 0 && e.key <= 9) || e.key === ".") handleNumber(e.key);
+  if (
+    e.key === "+" ||
+    e.key === "-" ||
+    e.key === "*" ||
+    e.key === "/" ||
+    e.key === "%"
+  )
+    handleSymbol(e.key);
+  if (e.key === "Enter" || e.key === "=") evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  textDisplay.textContent = number;
 }
